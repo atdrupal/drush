91adf60c3a671b079783924939995a62e77e19e8
Andy T. <andytruong@users.noreply.github.com>
2015-03-12 15:41:47 +0700

    Update TestDrushMaking.php

--- tests/TestDrushMaking.php
+++ tests/TestDrushMaking.php
@@ -2,24 +2,10 @@
 
 class TestDrushMaking extends PHPUnit_Framework_TestCase
 {
-    public function testDrupalMake()
+    public function testMakeResults()
     {
         $dir = dirname(__DIR__) . '/www';
         $this->assertFileExists($dir . '/www/install.php', "Found install.php file");
         $this->assertFileExists($dir . '/www/sites/all/modules/navbar/navbar.info', 'Found navbar module');
-    }
-
-    public function testProjectAPIMake()
-    {
-        $dir = dirname(__DIR__) . '/project-api';
-        $this->assertFileExists($dir . '/www/install.php', "Found install.php file");
-        $this->assertFileExists($dir . '/www/sites/all/modules/api/api.info', 'Found api module');
-    }
-
-    public function testProjectHeadLess()
-    {
-        $dir = dirname(__DIR__) . '/project-headless';
-        $this->assertFileExists($dir . '/www/install.php', "Found install.php file");
-        $this->assertFileExists($dir . '/www/sites/all/modules/restws/restws.info', 'Found restws module');
     }
 }

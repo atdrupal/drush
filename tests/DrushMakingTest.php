bd3ecf573862f35e479d3f87c60d48d75aa7d002
Andy T. <andytruong@users.noreply.github.com>
2015-03-25 12:12:13 +0700

    Update DrushMakingTest.php

--- tests/DrushMakingTest.php
+++ tests/DrushMakingTest.php
@@ -2,10 +2,20 @@
 
 class DrushMakingTest extends PHPUnit_Framework_TestCase
 {
-    public function testMakeResults()
+    /**
+     * @dataProvider provideMakeResults
+     */
+    public function testMakeResults($file, $msg)
     {
+        $this->assertFileExists($file, $msg);
+    }
+    
+    public function provideMakeResults() {
         $dir = dirname(__DIR__) . '/www';
-        $this->assertFileExists($dir . '/install.php', "Found install.php file");
-        $this->assertFileExists($dir . '/sites/all/modules/navbar/navbar.info', 'Found navbar module');
+        return array(
+            array($dir . '/install.php', "Found install.php file"),
+            array($dir . '/sites/all/modules/navbar/navbar.info', 'Found navbar module'),
+            array($dir . '/sites/all/modules/gd_react.module', 'Found gd_react module'),
+        );
     }
 }
